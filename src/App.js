ff94de5e552fd949144cfa7b5f21bccaeabd2829
Yael Yaffe <yaelyf@post.jce.ac.il>
2022-12-12 23:38:03 +0200

    HW

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Message from './Message';
+
 
 function App() {
+  const [password, setPassword] = useState("")
+  const [disable, setDisable] = useState(true)
+
+  function handlePassword(e){
+    setPassword(String(e.target.value))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <input type="password" value = {password} onChange = {handlePassword}></input>
+        <button disabled = {disable}>Submit!</button>
+        <Message password = {password} checkPassword = {handlePassword} setPassword = {setPassword} setDisable = {setDisable}/> 
+      </p>
     </div>
   );
 }
